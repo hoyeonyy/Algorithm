fa8b79922c5a554482ab175dd8a0397de5fa0e30
HoYeon <114469256+hoyeonyy@users.noreply.github.com>
2023-09-27 10:44:08 +0900

    [Bronze V] Title: 사칙연산, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// command shift l -> 단축기 전체 목
+		// command +, - -> 확대, 축소
+		// command d -> 한줄 삭제
+		// command option down -> 행단위 복사
+		// option up, down -> 행단위 이동
+		// command i -> 들여쓰기 맞춰주기
+		// command / -> 주석
+		// control command space -> 자동완성
+		
+		//import java.util.* -> 스캐너를 쓰기위한 임포트코드
+		//Scanner scanner = new Scanner(System.in); ->입력받는
+		//Math.round(4.52) -> 소수점 첫쨰자리에서 반올림
+		//a = scanner.nextInt();
+		//str.equals("yes") -> 문자열 str이 "yes"일
+		//Math.random() -> 0.0과 0.99999~사이의 임의의  double값 반
+		int a;
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
