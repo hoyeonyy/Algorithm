dd4e2a97edf112152e1f6b9e6ff1ecc7993c76f7
HoYeon <114469256+hoyeonyy@users.noreply.github.com>
2023-10-20 19:49:33 +0900

    [Bronze III] Title: 최소, 최대, Time: 496 ms, Memory: 91772 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,23 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		
-		int n = scan.nextInt();
-		int[] ar = new int[n];
-		for(int i = 0 ; i < n;i++) {
-			ar[i] = scan.nextInt();
-			
-		}
-		int max = -1000000, min = 1000000;
-		for(int i = 0 ; i <n;i++) {
-			if(max < ar[i])max = ar[i];
-			if(min > ar[i])min = ar[i];
-		}
-		System.out.printf("%d %d", min, max);
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n;
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ar[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i]= Integer.parseInt(st.nextToken());
+        }
+        
+        int max = -1000000, min = 1000000;
+        for(int i = 0 ; i <n;i++) {
+            if(max < ar[i])max = ar[i];
+            if(min > ar[i])min = ar[i];
+        }
+        System.out.printf("%d %d", min, max);
+    }
 
 }
